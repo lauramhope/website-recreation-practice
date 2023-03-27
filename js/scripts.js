0d335f014697b649c900edec2c6060203ed8f681
Laura Hope <ssteamlaura@gmail.com>
2023-03-26 20:47:16 -0700

    Remove H1 element via element.remove() method.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,12 @@
 //all code that accesses/manipulates the DOM needs to be in the load -- need to wait to run our DOM manipulating code until webpage has loaded
 
 window.addEventListener("load", function() { 
+  const removeH1 = document.querySelector("h1");
+  removeH1.remove();
+  
 
   // const form = document.getElementById("calculator");
   // form.addEventListener("submit", handleCalculation);
-
-  
 });
+
+
