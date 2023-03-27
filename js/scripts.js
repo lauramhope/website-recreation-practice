9df033abc86317224be3f99a538ae8fbeb2765e4
Laura Hope <ssteamlaura@gmail.com>
2023-03-26 20:41:48 -0700

    Add HTML template with external js/scripts.js and README.md files.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+//all code that accesses/manipulates the DOM needs to be in the load -- need to wait to run our DOM manipulating code until webpage has loaded
+
+window.addEventListener("load", function() { 
+
+  // const form = document.getElementById("calculator");
+  // form.addEventListener("submit", handleCalculation);
+
+  
+});
